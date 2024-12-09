72aa0690aa3fe933d4be18ecf71092326f8d8dbe
andyo <javabudd@gmail.com>
2024-12-08 22:10:56 -0700

    momenutm

--- lib/coin-trader.ts
+++ lib/coin-trader.ts
@@ -254,7 +254,7 @@ export default class CoinTrader {
       (trade.virtual_token_reserves + trade.token_amount);
 
     const volumeThreshold = 100_000_000_000_000;
-    const momentumThreshold = 10;
+    const momentumThreshold = 9;
     const priceImpactThreshold = 0.02;
 
     const volumeMetric = trade.token_amount > volumeThreshold;
